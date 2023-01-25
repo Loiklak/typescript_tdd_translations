5e2ba305ef7ba5f49f49f9a92b07c65540c4e121
Loïc Chau <loicch@theodo.fr>
2023-01-25 15:36:09 +0100

    9-handle_single_nested_traduction

--- src/TranslationKeysExtractor.ts
+++ src/TranslationKeysExtractor.ts
@@ -4,9 +4,15 @@ type TranslationShape = {
 
 type ValueOf<T> = T[keyof T];
 
-type Join<T, U> = T extends string ? U extends string ? `${T}.${U}` : never : never;
+type Join<T, U> = T extends string
+  ? U extends string
+    ? `${T}.${U}`
+    : never
+  : never;
 
 export type TranslationKeysExtractor<Translations> =
   Translations extends TranslationShape ? Extractor<Translations> : never;
 
-type Extractor<Translations> = ValueOf<Translations> extends string ? keyof Translations : ??;
+type Extractor<Translations> = ValueOf<Translations> extends string
+  ? keyof Translations
+  : Join<keyof Translations, TranslationKeysExtractor<ValueOf<Translations>>>;
