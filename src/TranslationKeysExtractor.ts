9c73865bab7b401b561818eb9d32c84bfcdbe9ac
Loïc Chau <loicch@theodo.fr>
2023-01-25 15:34:31 +0100

    8-introduce_join

--- src/TranslationKeysExtractor.ts
+++ src/TranslationKeysExtractor.ts
@@ -4,7 +4,9 @@ type TranslationShape = {
 
 type ValueOf<T> = T[keyof T];
 
+type Join<T, U> = T extends string ? U extends string ? `${T}.${U}` : never : never;
+
 export type TranslationKeysExtractor<Translations> =
   Translations extends TranslationShape ? Extractor<Translations> : never;
 
-type Extractor<Translations> = keyof Translations;
+type Extractor<Translations> = ValueOf<Translations> extends string ? keyof Translations : ??;
