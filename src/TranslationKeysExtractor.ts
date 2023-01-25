d35df8cf024aa87cc4a28ffedada517d159831ea
Loïc Chau <loicch@theodo.fr>
2023-01-25 15:32:08 +0100

    7-introduce_valueof

--- src/TranslationKeysExtractor.ts
+++ src/TranslationKeysExtractor.ts
@@ -2,6 +2,8 @@ type TranslationShape = {
   [key: string]: string | TranslationShape;
 };
 
+type ValueOf<T> = T[keyof T];
+
 export type TranslationKeysExtractor<Translations> =
   Translations extends TranslationShape ? Extractor<Translations> : never;
 
